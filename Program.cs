c467c89a82d5cc3ac13409e3929d5d71691962ee
Magnus Mutai <MagnusMq07@gmail.com>
2024-10-05 11:44:21 +0300

    Adding login endpoint

--- Program.cs
+++ Program.cs
@@ -2,6 +2,10 @@ using System.Security.Claims;
 using ANetCoreAngularAuth;
 
 var builder = WebApplication.CreateBuilder(args);
+
+build.Services.AddAuthentication("cookie")
+    .AddCookie("cookie");
+
 var app = builder.BuildWithSpa();
 
 var apiEndpoints = app.MapGroup("/api");
@@ -9,7 +13,18 @@ var apiEndpoints = app.MapGroup("/api");
 apiEndpoints.MapGet("/user", (ClaimsPrincipal user) => 
     user.Claims.ToDictionary(x => x.Type, x => x.Value));
 
-apiEndpoints.MapGet("/login", () => "Hello World!");
+// apiEndpoints.MapGet("/login", (LoginForm LoginForm) =>Results.SignIn (
+
+// )
+
+//  "Hello World!"{});
+
 apiEndpoints.MapGet("/register", () => "Hello World!");
 
 app.Run();
+
+public class LoginForm
+{
+    public string UserName { get; set; }
+    public string Password { get; set; }
+}
